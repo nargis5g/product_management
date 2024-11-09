0b748d4e5967e1d8591b1fb24b6a925006bc8fe3
Nargis <nargisjannat49@gmail.com>
2024-11-09 20:36:05 +0600

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -123,13 +123,19 @@
 
 
 
+
+
+
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nargis5g/product_management.git
+git push -u origin main
+
+
+
+
  
 
 
 
-
- README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/nargis5g/laravel_product.git
-git push -u origin main
